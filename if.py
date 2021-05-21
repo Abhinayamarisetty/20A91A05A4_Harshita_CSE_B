e4d303538f3fe2c473dd1ef3bd057220b5b84388
Harshitamattaparti <84396867+Harshitamattaparti@users.noreply.github.com>
2021-05-21 10:29:27 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,16 @@
+a=9
+if a<10:
+    print('a is less than 10')
+    print('hi....')
+
+OUTPUT:
+a is less than 10
+hi....
+
+a=19
+if a<10:
+    print('a is less than 10')
+    print('hi....')
+
+OUTPUT:
+
