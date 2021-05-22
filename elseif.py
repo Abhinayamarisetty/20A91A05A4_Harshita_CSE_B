6fa86222a023665b2bd54cf3d3e681dcbbc7472c
Harshitamattaparti <84396867+Harshitamattaparti@users.noreply.github.com>
2021-05-22 14:28:20 +0530

    Add files via upload

---
+++ elseif.py
@@ -0,0 +1,22 @@
+num=int(input("Enter number"))
+if (num%2!=0)and(num<=20):
+    print("Weird")
+elif (num%2==0)and(2<=num<=5):
+    print("Not weird")
+elif (num%2==0)and(6<=num<=20):
+    print("Weird")
+else:
+    print("Not weird")
+
+OUTPUT:
+Enter number1
+Weird
+
+Enter number4
+Not weird
+
+Enter number15
+Weird
+
+Enter number36
+Not weird
