04b023dfeb38831258daf533c28a8ff9ff289ee6
Harshitamattaparti <84396867+Harshitamattaparti@users.noreply.github.com>
2021-05-22 13:45:20 +0530

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,12 @@
+num=int(input("Enter number"))
+if num%2==0:
+    print(num,"is an Even number")
+else:
+    print(num,"is an Odd number")
+
+OUTPUT:
+Enter number387
+387 is an Odd number
+
+Enter number45678924
+45678924 is an Even number
