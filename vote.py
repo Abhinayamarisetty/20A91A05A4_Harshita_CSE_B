91899ca5fb184f008b6347ee5706db7b25a5eb06
Harshitamattaparti <84396867+Harshitamattaparti@users.noreply.github.com>
2021-05-22 12:25:32 +0530

    Add files via upload

---
+++ vote.py
@@ -0,0 +1,14 @@
+name=input("Enter name")
+age=int(input("Enter age"))
+if (age<18):
+    print("Sorry",name)
+    print("You are Not eligible to vote")
+else:
+    print("Hello",name)
+    print("You are Eligible to vote")
+
+OUTPUT:
+Enter nameHarshita
+Enter age38
+Hello Harshita
+You are Eligible to vote
